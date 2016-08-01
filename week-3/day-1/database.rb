a6b999f13b52d2fe461cd7d780c924f9e2526df4
Gavin Stark <gavin@gstark.com>
2016-08-01 09:42:39 -0400

    Basic search function working

--- week-3/day-1/database.rb
+++ week-3/day-1/database.rb
@@ -5,19 +5,22 @@ end
 class Database
   def initialize
     @people = []
+
     person = Person.new
     person.name = "Gavin"
     person.salary = 1_000_000
     person.position = "Instructor"
     @people << person
 
+    person = Person.new
     person.name = "Jason"
-    person.salary = 1_000_000
+    person.salary = 2_000_000
     person.position = "Instructor"
     @people << person
 
+    person = Person.new
     person.name = "Toni"
-    person.salary = 1_000_000
+    person.salary = 3_000_000
     person.position = "CD"
     @people << person
   end
@@ -28,6 +31,17 @@ class Database
     name_to_find = gets.chomp
 
     # look for that person in the database
+    # use some enumerable method that gives me back
+    # the first person who's name matches the name
+    # I am looking for
+    found_person = @people.find { |person| name_to_find == person.name }
+
+    if found_person != nil
+      puts "Details about #{found_person.name}. They make #{found_person.salary} dollars"
+    else
+      puts "Sorry..."
+    end
+
     # if found
     #   print details
     # else
