16fc0e823ae1f4714a596a9623caa627a7c2c671
Gavin Stark <gavin@gstark.com>
2016-08-17 13:56:37 -0400

    The rest of adding auth to our tiy sports rails app

--- week-5/day-3/tiyrailssports/app/controllers/players_controller.rb
+++ week-5/day-3/tiyrailssports/app/controllers/players_controller.rb
@@ -1,5 +1,6 @@
 class PlayersController < ApplicationController
   before_action :set_player, only: [:show, :edit, :update, :destroy]
+  before_action :authorize!
 
   # GET /players
   # GET /players.json
--- week-5/day-3/tiyrailssports/config/routes.rb
+++ week-5/day-3/tiyrailssports/config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
+
   resources :players
   resources :teams
-  resource :session, only: [:new, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
