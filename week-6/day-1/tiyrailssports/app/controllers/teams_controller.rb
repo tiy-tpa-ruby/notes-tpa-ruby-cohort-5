3050d9950c311d8035ba845ca1219df5869d9675
Gavin Stark <gavin@gstark.com>
2016-08-23 05:45:25 -0400

    Fixing authenticate method

--- week-6/day-1/tiyrailssports/app/controllers/teams_controller.rb
+++ week-6/day-1/tiyrailssports/app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy]
-  before_action :authorize!
+  before_action :authenticate!
 
   # GET /teams
   # GET /teams.json
