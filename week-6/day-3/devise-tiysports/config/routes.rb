a447615545df300522f43da9b14a33a919008eb3
Gavin Stark <gavin@gstark.com>
2016-08-24 11:30:13 -0400

    [Optional Step] generate specific controllers for devise - only do this if you need extra work done when creating users / destroying users / etc

--- week-6/day-3/devise-tiysports/config/routes.rb
+++ week-6/day-3/devise-tiysports/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :players
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'players#index'
