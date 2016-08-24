9283299f2d06fc1748f6e80bbcdd4261c985effa
Gavin Stark <gavin@gstark.com>
2016-08-24 11:56:59 -0400

    Adds user name to the user model, allows it to be provided at sign up, uses it in the application layout, also tries to turn on lockable
    Lockable works but we are missing the warnings on the page

--- week-6/day-3/devise-tiysports/db/schema.rb
+++ week-6/day-3/devise-tiysports/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824150734) do
+ActiveRecord::Schema.define(version: 20160824153625) do
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160824150734) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
