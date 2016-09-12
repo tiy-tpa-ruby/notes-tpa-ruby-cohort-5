b4850a65c07b1dfccf43e7774a903525ada64fe1
Gavin Stark <gavin@gstark.com>
2016-09-12 09:56:05 -0400

    Week 9 - Day 1 - Uses URI escape to allow multi word searches

--- week-9/day-1/APIs-jQuery/app/controllers/definitions_controller.rb
+++ week-9/day-1/APIs-jQuery/app/controllers/definitions_controller.rb
@@ -2,7 +2,8 @@ class DefinitionsController < ApplicationController
   def show_def
     @term = params[:term]
 
-    @results = HTTParty.get("http://api.wordnik.com:80/v4/word.json/#{@term}/definitions?limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    url = URI.escape("http://api.wordnik.com:80/v4/word.json/#{URI.escape(@term)}/definitions?limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    @results = HTTParty.get(url)
 
     @definitions = @results.map { |result| result["text"] }
   end
